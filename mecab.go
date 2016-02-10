f0418ff4ee29174d25ed6759144fe2207975226a
Ichinose Shogo <shogo82148@gmail.com>
2016-02-10 21:08:31 +0900

    force enable --allocate-sentence option

--- mecab.go
+++ mecab.go
@@ -6,7 +6,6 @@ import "C"
 import (
 	"errors"
 	"fmt"
-	"strings"
 	"unsafe"
 )
 
@@ -16,19 +15,20 @@ type MeCab struct {
 
 func New(args map[string]string) (*MeCab, error) {
 	var model *C.mecab_model_t
-	if len(args) > 0 {
-		opts := make([]*C.char, 0, len(args))
-		for k, v := range args {
-			opt := C.CString(fmt.Sprintf("--%s=%s", strings.Replace(k, "_", "-", -1), v))
-			defer C.free(unsafe.Pointer(opt))
-			opts = append(opts, opt)
-		}
-		model = C.mecab_model_new(C.int(len(opts)), (**C.char)(&opts[0]))
-	} else {
-		opt := C.CString("")
+
+	// build the argument
+	opts := make([]*C.char, 0, len(args)+1)
+	opt := C.CString("--allocate-sentence")
+	defer C.free(unsafe.Pointer(opt))
+	opts = append(opts, opt)
+	for k, v := range args {
+		opt := C.CString(fmt.Sprintf("--%s=%s", k, v))
 		defer C.free(unsafe.Pointer(opt))
-		model = C.mecab_model_new2(opt)
+		opts = append(opts, opt)
 	}
+
+	// create new MeCab model
+	model = C.mecab_model_new(C.int(len(opts)), (**C.char)(&opts[0]))
 	if model == nil {
 		return nil, errors.New("mecab_model is not created.")
 	}
