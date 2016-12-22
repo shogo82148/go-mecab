3453abab3ead2f0489b8998c65a30dddb2480605
Ichinose Shogo <shogo82148@gmail.com>
2016-12-22 11:59:07 +0900

    auto destory by SetFinalizer

--- model.go
+++ model.go
@@ -6,6 +6,7 @@ import "C"
 import (
 	"errors"
 	"fmt"
+	"runtime"
 	"unsafe"
 )
 
@@ -13,7 +14,7 @@ type Model struct {
 	model *C.mecab_model_t
 }
 
-func NewModel(args map[string]string) (Model, error) {
+func NewModel(args map[string]string) (*Model, error) {
 	// build the argument
 	opts := make([]*C.char, 0, len(args)+1)
 	opt := C.CString("mecab")
@@ -37,35 +38,45 @@ func NewModel(args map[string]string) (Model, error) {
 	// create new MeCab model
 	model := C.mecab_model_new(C.int(len(opts)), (**C.char)(&opts[0]))
 	if model == nil {
-		return Model{}, errors.New("mecab_model is not created.")
+		return nil, errors.New("mecab_model is not created.")
 	}
+	m := &Model{model: model}
+	runtime.SetFinalizer(m, (*Model).Destroy)
 
-	return Model{
-		model: model,
-	}, nil
+	return m, nil
 }
 
-func (m Model) Destroy() {
+func (m *Model) Destroy() {
+	if m == nil || m.model == nil {
+		return
+	}
 	C.mecab_model_destroy(m.model)
+	m.model = nil
 }
 
-func (m Model) NewMeCab() (MeCab, error) {
+func (m *Model) NewMeCab() (*MeCab, error) {
 	mecab := C.mecab_model_new_tagger(m.model)
 	if mecab == nil {
-		return MeCab{}, errors.New("mecab is not created.")
+		return nil, errors.New("mecab is not created.")
 	}
-	return MeCab{mecab: mecab}, nil
+	m2 := &MeCab{mecab: mecab}
+	runtime.SetFinalizer(m2, (*MeCab).Destroy)
+
+	return m2, nil
 }
 
-func (m Model) NewLattice() (Lattice, error) {
+func (m *Model) NewLattice() (*Lattice, error) {
 	lattice := C.mecab_model_new_lattice(m.model)
 	if lattice == nil {
-		return Lattice{}, errors.New("lattice is not created")
+		return nil, errors.New("lattice is not created")
 	}
-	return Lattice{lattice: lattice}, nil
+	l := &Lattice{lattice: lattice}
+	runtime.SetFinalizer(l, (*Lattice).Destroy)
+
+	return l, nil
 }
 
 // Swap replaces the model by the othor model.
-func (m Model) Swap(m2 Model) {
+func (m *Model) Swap(m2 *Model) {
 	C.mecab_model_swap(m.model, m2.model)
 }
