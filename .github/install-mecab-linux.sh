912d9b3cbefe16ee9f1e689baa25bb0129e776b2
Ichinose Shogo <shogo82148@gmail.com>
2020-02-04 04:30:28 +0900

    Hello GitHub Actions

---
+++ .github/install-mecab-linux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# from https://gist.github.com/dtan4/351d031bec0c3d45cd8f
+# see also http://qiita.com/dtan4/items/c6a087666296fbd5fffb
+
+set -uxe
+
+TMPDIR=$(mktemp -d)
+trap 'rm -rfv "$TMPDIR"' EXIT
+
+MECAB_VERSION=1.10.4
+IPADIC_VERSION=2.7.0-20070801
+# install mecab
+cd "$TMPDIR"
+curl -o mecab.tar.gz -sSL "https://github.com/shogo82148/mecab/releases/download/v$MECAB_VERSION/mecab-$MECAB_VERSION.tar.gz"
+tar zxfv mecab.tar.gz
+cd "mecab-$MECAB_VERSION"
+./configure --enable-utf8-only
+make
+make check
+sudo make install
+sudo ldconfig
+
+cd "$TMPDIR"
+curl -o mecab-ipadic.tar.gz -sSL "https://github.com/shogo82148/mecab/releases/download/v$MECAB_VERSION/mecab-ipadic-$IPADIC_VERSION.tar.gz"
+tar zxfv mecab-ipadic.tar.gz
+cd "mecab-ipadic-$IPADIC_VERSION"
+./configure --with-charset=utf8
+make
+sudo make install
