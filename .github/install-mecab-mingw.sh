b424497d46241cfe740da93eecdb8bd1d1353a77
Ichinose Shogo <shogo82148@gmail.com>
2020-02-14 09:41:41 +0900

    test on MinGW

---
+++ .github/install-mecab-mingw.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -uxe
+
+# setup MSYS2
+pacman-key --init
+pacman-key --populate msys2
+pacman -Sy
+pacman -S --verbose --noconfirm --noprogressbar --needed make mingw-w64-x86_64-gcc mingw-w64-x86_64-libtool
+
+TMPDIR=$(mktemp -d)
+trap 'rm -rfv "$TMPDIR"' EXIT
+
+MECAB_VERSION=0.996.2
+IPADIC_VERSION=2.7.0-20070801
+# install mecab
+cd "$TMPDIR"
+curl -o mecab.tar.gz -sSL "https://github.com/shogo82148/mecab/releases/download/v$MECAB_VERSION/mecab-$MECAB_VERSION.tar.gz"
+tar zxfv mecab.tar.gz
+cd "mecab-$MECAB_VERSION"
+./configure --enable-utf8-only
+make
+make check
+sudo make install
+sudo ldconfig
+
+cd "$TMPDIR"
+curl -o mecab-ipadic.tar.gz -sSL "https://github.com/shogo82148/mecab/releases/download/v$MECAB_VERSION/mecab-ipadic-$IPADIC_VERSION.tar.gz"
+tar zxfv mecab-ipadic.tar.gz
+cd "mecab-ipadic-$IPADIC_VERSION"
+./configure --with-charset=utf8
+make
+sudo make install
+
+echo "::set-env name=CGO_LDFLAGS::$(mecab-config --libs)"
+echo "::set-env name=CGO_CFLAGS::-I$(mecab-config --inc-dir)"
